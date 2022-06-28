777192ea3cb0b90f3c82cf00956feaee0156e184
KrasMax <maximkraskovskiy@gmail.com>
2022-06-28 23:28:28 +0300

    complited tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const subArr = String(n).split('');
+    let arr = ""
+    let subRes = ''
+    if (subArr[0] === '-') {
+        subArr.shift();
+
+        arr = subArr.reverse();
+
+    } else arr = subArr.reverse();
+
+    arr.forEach(e => {
+        subRes += e
+    });
+
+    return Number(subRes);
 }
